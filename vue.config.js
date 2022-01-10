0e4af64f707e8f71dab52f42612abef47a65aaa7
yzc <510666772@qq.com>
2022-01-10 16:52:45 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+mobule.exports= {
+  configureWebpack:{
+    resolve:{
+      alias: {
+        'assets' :'@assets',
+        'common' :'@common',
+        'components' :'@components',
+        'network' :'@network',
+        'views' :'@views',
+      }
+    }
+  }
+}
